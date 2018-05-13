ef2a6341ad395f5608d6e09f8f2778184d4bb79b
realdennis <evolon688@gmail.com>
2018-05-13 12:41:29 +0800

    add some test case to increase code coverage

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 var equal = assert.equal;
 
+//Triangle
 var tri = require('../tri/tri');
 describe('Triangle test',()=>{
     describe('Code Coverage',()=>{
@@ -11,6 +12,10 @@ describe('Triangle test',()=>{
             equal(tri(5,5,5),'Equilateral');
             equal(tri(-1,-1,-1),'Not a triangle');
             equal(tri(5,5,10),'Not a triangle');
+            equal(tri(3, 3, 5), "Isosceles");
+            equal(tri(3, 5, 3), "Isosceles");
+            equal(tri(5, 3, 3), "Isosceles");
+            equal(tri(3, 4, 5), "Scalene");
         });
         it('c2',()=>{
             equal(tri(5,5,5),'Equilateral');
