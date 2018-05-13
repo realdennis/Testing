5225d597ae43ae8b087aa350a429c90ff682ba84
realdennis <evolon688@gmail.com>
2018-05-13 14:19:51 +0800

    path testing done

--- test/commission_unittest.js
+++ test/commission_unittest.js
@@ -27,10 +27,51 @@ describe('Next Date test',()=>{
             equal(com(20,20,20),260.0);
         })
         it('c1',()=>{
+            equal(com(-1,20,20),'Out of range value');
+            equal(com(0,20,20),'Out of range value');
+            equal(com(0,20,20),'Out of range value');
+            equal(com(71,20,20),'Out of range value');
+            equal(com(20,0,20),'Out of range value');
+            equal(com(20,20,91),'Out of range value');
+            //sales <= 1000
+            equal(com(1,1,1),10.0);
+            //sales >1000 & sales <=1800;
+            equal(com(1,20,20),121.75);
+            //sales>1800
+            equal(com(20,20,20),260.0);
         });
         it('c2',()=>{
+            //terminated
+            equal(com(-1,20,20),'Out of range value');
+            //invalid
+            equal(com(0,20,20),'Out of range value');
+            equal(com(0,20,20),'Out of range value');
+            equal(com(71,20,20),'Out of range value');
+            equal(com(20,0,20),'Out of range value');
+            equal(com(20,20,91),'Out of range value'); 
+            //sales <= 1000
+            equal(com(1,1,1),10.0);
+            //sales >1000 & sales <=1800;
+            equal(com(1,20,20),121.75);
+            //sales>1800
+            equal(com(20,20,20),260.0);
+
         });
-        it('MCDC',()=>{
+        it('MCDC',()=>{            
+            //terminated
+            equal(com(-1,20,20),'Out of range value');
+            //invalid
+            equal(com(0,20,20),'Out of range value');
+            equal(com(0,20,20),'Out of range value');
+            equal(com(71,20,20),'Out of range value');
+            equal(com(20,0,20),'Out of range value');
+            equal(com(20,20,91),'Out of range value'); 
+            //sales <= 1000
+            equal(com(1,1,1),10.0);
+            //sales >1000 & sales <=1800;
+            equal(com(1,20,20),121.75);
+            //sales>1800
+            equal(com(20,20,20),260.0);
         })
     })
 })
