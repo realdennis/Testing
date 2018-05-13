0b2c57750d48ce4380b47f6851b744eb218c47dd
realdennis <evolon688@gmail.com>
2018-05-13 12:55:45 +0800

    rename

--- test/tri_unittest.js
+++ test/tri_unittest.js
@@ -1,8 +1,7 @@
 var assert = require('assert');
 var equal = assert.equal;
 
-//Triangle
-var tri = require('../tri/tri');
+var tri = require('../Triangle/tri');
 describe('Triangle test',()=>{
     describe('Code Coverage',()=>{
         it('c0',()=>{
@@ -21,8 +20,25 @@ describe('Triangle test',()=>{
             equal(tri(5,5,5),'Equilateral');
             equal(tri(-1-1-1),'Not a triangle');
             equal(tri(5,5,10),'Not a triangle');
+            equal(tri(3, 3, 5), "Isosceles");
+            equal(tri(3, 5, 3), "Isosceles");
+            equal(tri(5, 3, 3), "Isosceles");
+            equal(tri(3, 4, 5), "Scalene");
         });
         it('MCDC',()=>{
+            equal(tri(5, 5, 5), "Equilateral");
+            equal(tri(0, 5, 5), "Not a triangle");
+            equal(tri(201, 5, 5), "Not a triangle");
+            equal(tri(5, 0, 5), "Not a triangle");
+            equal(tri(5, 201, 5), "Not a triangle");
+            equal(tri(5, 5, 0), "Not a triangle");
+            equal(tri(5, 5, 201), "Not a triangle");
+            equal(tri(-1, -1, -1), "Not a triangle");
+
+            equal(tri(5, 5, 10), "Not a triangle");
+            equal(tri(5, 10, 5), "Not a triangle");
+            equal(tri(10, 5, 5), "Not a triangle");
+
             equal(tri(3,3,5),'Isosceles');
             equal(tri(3,5,3),'Isosceles');
             equal(tri(5,3,3),'Isosceles');
